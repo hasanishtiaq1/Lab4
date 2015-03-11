d6c0d93a9dc34d81821a4301488a6806d7ad4a7d
hasanishtiaq1 <hasan.coder1@gmail.com>
2015-03-11 21:27:32 +0500

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<?php$fname = $_POST['firstname']; 
+echo($fname); 
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>If you click "Submit", the form-data will be sent to a page called "action_page.php".</p>
+
+</body>
+</html>
